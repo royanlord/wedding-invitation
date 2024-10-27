1b6be339e4aba3f68164c12c8e1dec2c2d7c8381
royanlord <royanfarid32@gmail.com>
2024-10-28 06:59:32 +0700

    set aos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 'use client'
 import CoverInvitation from "@/components/ui/cover/CoverInvitation";
+import LayoutSection from "@/components/ui/LayoutSection";
 import WelcomingCover from "@/components/ui/welcoming/WelcomingCover";
-import WelcomingSection from "@/components/ui/welcoming/WelcomingSection";
 import useOpen from "@/stores/store";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
 
 export default function Home() {
     const { isOpen } = useOpen();
+
+    useEffect(() => {
+        AOS.init()
+    }, [])
+
     return (
         <div className="w-full lg:flex">
             <CoverInvitation />
             <div className="lg:ml-[66.6667%] w-full h-screen">
                 {isOpen === true ? (
-                    <div className="w-full">
-                      <WelcomingSection />
+                    <div className="w-full" data-aos="fade-up" data-aos-duration="1000">
+                        <LayoutSection />
                     </div>
                 ) : (
                     <WelcomingCover />
