adeff54f3fe3c02acac5ed0859e1bb055c54f0b5
royanlord <royanfarid32@gmail.com>
2024-10-28 07:47:44 +0700

    modify scroll to begin button

--- src/components/ui/welcoming/WelcomingSection.tsx
+++ src/components/ui/welcoming/WelcomingSection.tsx
@@ -55,7 +55,7 @@ const WelcomingSection = ({ id }: WelcomingSectionProps) => {
 
                 <div className="pt-48 w-full relative z-10">
                     <div 
-                        className="float-end pe-10 flex items-center gap-2 cursor-pointer"
+                        className="lg:justify-end justify-center lg:pe-10 flex items-center gap-2 cursor-pointer"
                         onClick={() => window.location.href = "#section2"}
                     >
                         <span className="text-white font-semibold">
@@ -64,8 +64,8 @@ const WelcomingSection = ({ id }: WelcomingSectionProps) => {
                         <svg 
                             xmlns="http://www.w3.org/2000/svg" 
                             viewBox="0 0 448 512"
-                            width="28" 
-                            height="28" 
+                            width="25" 
+                            height="25" 
                             fill="white"
                         >
                             <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
